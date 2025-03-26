f534f0ff542c5fca288c2cb5bb35c90fd9ffdf7c
Cris-Dl <deleonperezcristhianestuardo@gmail.com>
2025-03-26 11:22:26 -0600

    Add files via upload

---
+++ Hoja de trabajo 7.py
@@ -0,0 +1,26 @@
+#Ejercicio 1
+#i=0
+#for i in range(1,11,2):
+    #print("Los números impares del 0 al 10 son:", i)
+
+#Ejercicio 2
+#x=1
+#num=x
+#while x<11:
+    #if x%2 !=0:
+        #print("El número es:", x)
+    #x+=1
+
+# Escenario 1
+# palabra=str(input("Escriba alguna palabra: "))
+# while palabra!="chupacabra":
+#     palabra=str(input("Vuelva a escribir una palabra: "))
+# print("Has salido del bucle")
+
+#  Escenario 2
+# palabra=str(input("Ingrese una palabra: "))
+# palabra=palabra.upper()
+# for letra in palabra:
+#     if letra in ["A", "E", "I", "O", "U"]:
+#         continue
+#     print(letra)
