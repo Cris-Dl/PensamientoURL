f9cda4e6a37a0a9af6a24a52de4337622f4288e7
Cris-Dl <deleonperezcristhianestuardo@gmail.com>
2025-05-21 11:31:10 -0600

    Hoja de trabajo 13.py

--- Hoja de trabajo 13.py
+++ Hoja de trabajo 13.py
@@ -3,3 +3,14 @@ def tablero(matriz):
     for i in matriz:
         print(i)
     print()
+
+def contador (fila, columna, matriz):
+    vecinos = 0
+    filas = len(matriz)
+    columnas = len(matriz[0])
+
+    if columna -1 >= 0:
+        vecinos += matriz[fila][columna - 1]
+    if columna + 1 < columnas:
+        vecinos += matriz[fila][columna+1]
+    if fila - 1 >= 
